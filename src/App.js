6dc5a39485278825b0b723c5c7eb5294ecba6cee
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-05 17:20:17 +0100

    added header component

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import Container from '@material-ui/core/Container'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 
 function App() {
 	return (
-		<Container>
-			<Switch>
-				<Route
-					exact
-					path="/"
-					component={HomePage}
-				/>
-				<Route
-					path="/shop"
-					component={ShopPage}
-				/>
-			</Switch>
-		</Container>
+		<>
+			<Header />
+			<Container>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						component={HomePage}
+					/>
+					<Route
+						path="/shop"
+						component={ShopPage}
+					/>
+				</Switch>
+			</Container>
+		</>
 	)
 }
 
