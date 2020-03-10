1497cbc8a1527b94b26f35fca27a16ca358115c0
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-10 09:39:29 +0100

    added redux functionality into cart component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,7 @@ import { ReactComponent as Logo } from '../../assets/codesandbox.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
 	<div className="header">
 		<Link to="/" className="logo-container">
 			<Logo className="logo" />
@@ -40,12 +40,16 @@ const Header = ({ currentUser }) => (
 			)}
 			<CartIcon />
 		</div>
-		<CartDropdown />
+		{!hidden && <CartDropdown />}
 	</div>
 )
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = ({
+	user: { currentUser },
+	cart: { hidden }
+}) => ({
+	currentUser,
+	hidden
 })
 
 export default connect(mapStateToProps)(Header)
