2a766fa6bf0563a39740b798d8b8f8b904fe6978
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-20 12:49:53 +0100

    persisting state into localtorage with redux-persist

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
 
-import store from './redux/store'
+import { store, persistor } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 import App from './App'
@@ -13,7 +14,9 @@ import App from './App'
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
