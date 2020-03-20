0bb9a09288511d05c736fd821c1e10d991a94326
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-20 16:16:50 +0100

    added dynamic routing with ownProps and selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
 import './collection.styles.scss'
 
-const CollectionPage = ({ match }) => {
-	console.log('match from category', match)
+const CollectionPage = ({ collection }) => {
+	console.log('coolection', collection)
 	return (
 		<div className="collection-page">
 			<h2>Category Page</h2>
@@ -12,4 +14,12 @@ const CollectionPage = ({ match }) => {
 	)
 }
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+	collection: selectCollection(
+		ownProps.match.params.collectionId
+	)(state)
+})
+
+export default connect(mapStateToProps)(
+	CollectionPage
+)
