669959ce8e2b5cf8a8ad4ec4207a0cdf65117446
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-20 17:02:29 +0100

    added route push on title press in collection preview component

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
 import CollectionItem from '../collection-item/collection-item.component'
+import { useHistory } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({ title, items }) => (
-	<div className="collection-preview">
-		<h1 className="title">
-			{title.toUpperCase()}
-		</h1>
+const CollectionPreview = ({
+	title,
+	items,
+	routeName
+}) => {
+	const history = useHistory()
+	return (
+		<div className="collection-preview">
+			<h1
+				className="title"
+				onClick={() =>
+					history.push(
+						`shop/${routeName}`
+					)
+				}
+			>
+				{title.toUpperCase()}
+			</h1>
 
-		<Grid
-			className="preview"
-			container
-			spacing={2}
-		>
-			{items
-				.filter(
-					(item, index) => index < 4
-				)
-				.map(item => (
-					<Grid item xs={6} sm={3}>
-						<CollectionItem
-							key={item.id}
-							item={item}
-						/>
-					</Grid>
-				))}
-		</Grid>
-	</div>
-)
+			<Grid
+				className="preview"
+				container
+				spacing={2}
+			>
+				{items
+					.filter(
+						(item, index) => index < 4
+					)
+					.map(item => (
+						<Grid item xs={6} sm={3}>
+							<CollectionItem
+								key={item.id}
+								item={item}
+							/>
+						</Grid>
+					))}
+			</Grid>
+		</div>
+	)
+}
 
 export default CollectionPreview
