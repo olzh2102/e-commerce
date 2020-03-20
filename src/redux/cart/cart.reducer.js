230e53b4e6bcfb3746a577877f2e9578ab156b1c
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-20 11:28:43 +0100

    remove items from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -25,6 +25,16 @@ const cartReducer = (
 				)
 			}
 
+		case CartActionTypes.CLEAR_ITEM_FROM_CART:
+			return {
+				...state,
+				cartItems: state.cartItems.filter(
+					cartItem =>
+						cartItem.id !==
+						action.payload.id
+				)
+			}
+
 		default:
 			return state
 	}
