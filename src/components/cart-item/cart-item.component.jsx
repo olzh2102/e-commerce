0a73189f5660d5f87c434598cb9d599813a9c2fc
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-22 12:13:26 +0100

    replaced scss with styled components

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-
-import './cart-item.styles.scss'
+import {
+	CartItemContainer,
+	ItemDetails
+} from './cart-item.styles'
 
 const CartItem = ({
 	item: { imageUrl, price, name, quantity }
 }) => (
-	<div className="cart-item">
+	<CartItemContainer>
 		<img src={imageUrl} alt="image" />
-		<div className="item-details">
+		<ItemDetails>
 			<span className="name">{name}</span>
 			<span className="price">
 				{quantity} x ${price}
 			</span>
-		</div>
-	</div>
+		</ItemDetails>
+	</CartItemContainer>
 )
 
 export default CartItem
