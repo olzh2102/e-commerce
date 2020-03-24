b9a7a13a7051d42b4f5233a8a73064c11e1a1121
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-24 14:54:01 +0100

    trying out redux-saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,13 @@
+import { takeEvery } from 'redux-saga/effects'
+import ShopActionTypes from './shop.types'
+
+export function* fetchCollectionsAsync() {
+	yield console.log('I am fired, you mofo')
+}
+
+export function* fetchCollectionsStart() {
+	yield takeEvery(
+		ShopActionTypes.FETCH_COLLECTIONS_START,
+		fetchCollectionsAsync
+	)
+}
