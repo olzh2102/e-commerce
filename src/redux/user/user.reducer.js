c02f4275d8015a412d0dd892aea2cad47509733b
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-25 15:56:55 +0100

    moving google and email signin to saga - created according reducers

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,7 +1,8 @@
 import { UserActionTypes } from './user.types'
 
 const INITIAL_STATE = {
-	currentUser: null
+	currentUser: null,
+	error: null
 }
 
 const userReducer = (
@@ -9,10 +10,19 @@ const userReducer = (
 	action
 ) => {
 	switch (action.type) {
-		case UserActionTypes.SET_CURRENT_USER:
+		case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
+		case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
 			return {
 				...state,
+				error: null,
 				currentUser: action.payload
+			}
+
+		case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
+		case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
+			return {
+				...state,
+				error: action.payload
 			}
 
 		default:
