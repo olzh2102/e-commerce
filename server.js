feee8d94f6efe0b5e8e799ef491efb4327adbe3f
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-28 19:42:20 +0100

    handling production mode in serverjs

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(
+		express.static(
+			path.join(__dirname, 'client/build')
+		)
+	)
+
+	app.get('*', (req, res) => {
+		res.sendFile(
+			path.join(
+				__dirname,
+				'client/build',
+				'index.html'
+			)
+		)
+	})
+}
+
+app.listen(PORT, () =>
+	console.log(`Server running on port ${PORT}`)
+)
