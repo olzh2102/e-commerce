bdd07e1efadd973ee65386d94675135233b7ac44
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-03-29 11:20:23 +0200

    payment route

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
+const stripe = require('stripe')(
+	process.env.STRIPE_SECRET_KEY
+)
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
@@ -36,3 +40,27 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(PORT, () =>
 	console.log(`Server running on port ${PORT}`)
 )
+
+app.post('/payment', (req, res) => {
+	const body = {
+		source: req.body.token.id,
+		amount: req.body.amount,
+		currency: 'usd'
+	}
+
+	stripe.charges.create(
+		body,
+		(stripeErr, stripeRes) => {
+			if (stripeErr) {
+				res.status(500).json({
+					error: stripeErr
+				})
+			}
+
+			res.status(200).json({
+				success: true,
+				data: stripeRes
+			})
+		}
+	)
+})
