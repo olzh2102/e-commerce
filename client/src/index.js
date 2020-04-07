f00e69378e51ee140da8425bcec9b2b20359d120
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-07 12:05:57 +0200

    updating code to be pwa compliant, added serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux'
 
 import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import * as serviceWorker from './serviceWorker'
 
 import './index.css'
 import App from './App'
@@ -21,3 +22,5 @@ ReactDOM.render(
 	</Provider>,
 	document.getElementById('root')
 )
+
+serviceWorker.register()
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 const compression = require('compression')
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
@@ -18,7 +19,7 @@ const PORT = process.env.PORT || 4000
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
@@ -39,9 +40,21 @@ if (process.env.NODE_ENV === 'production') {
 	})
 }
 
-app.listen(PORT, () =>
+app.listen(PORT, (error) => {
+	if (error) throw error
 	console.log(`Server running on port ${PORT}`)
-)
+})
+
+app.get('/service-worker.js', (req, res) => {
+	res.sendFile(
+		path.resolve(
+			__dirname,
+			'..',
+			'build',
+			'service-worker.js'
+		)
+	)
+})
 
 app.post('/payment', (req, res) => {
 	const body = {
